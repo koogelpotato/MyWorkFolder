37c42a34c46a6d27bb34e14d88c29670410cfb94
koogel <koogelpotato@gmail.com>
2023-04-04 01:11:55 +0300

    added CTest for HelloWorld

---
+++ 01-Homework/HelloWorld/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/koogel/.config/MyWorkFolder/01-Homework/HelloWorld
+# Build directory: /home/koogel/.config/MyWorkFolder/01-Homework/HelloWorld/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hello_world_test "/home/koogel/.config/MyWorkFolder/01-Homework/HelloWorld/build/hello_world")
+set_tests_properties(hello_world_test PROPERTIES  _BACKTRACE_TRIPLES "/home/koogel/.config/MyWorkFolder/01-Homework/HelloWorld/CMakeLists.txt;12;add_test;/home/koogel/.config/MyWorkFolder/01-Homework/HelloWorld/CMakeLists.txt;0;")
