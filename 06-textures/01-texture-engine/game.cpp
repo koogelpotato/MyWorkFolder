de16f278a8c0139827f05a23f603786870b3e591
koogel <koogelpotato@gmail.com>
2023-05-21 12:08:18 +0300

    testing input

--- 06-textures/01-texture-engine/game.cpp
+++ 06-textures/01-texture-engine/game.cpp
@@ -1,10 +1,13 @@
 #include "game.hpp"
 #include "resource-manager.hpp"
 #include "sprite-renderer.hpp"
+#include "game-object.hpp"
+
 
 
 
 Sprite_Renderer  *renderer;
+Game_Object *player;
 
 
 Game::Game(unsigned int width, unsigned int height) 
@@ -16,6 +19,7 @@ Game::Game(unsigned int width, unsigned int height)
 Game::~Game()
 {
     delete renderer;
+    delete player;
 }
 
 void Game::init_game()
@@ -31,7 +35,11 @@ void Game::init_game()
     renderer = new Sprite_Renderer(Resource_Manager::get_shader("sprite"));
 
     Resource_Manager::load_texture("/home/koogel/.config/MyWorkFolder/06-textures/01-texture-engine/skull.png", true, "skull");
+    
+    glm::vec2 playerPos = glm::vec2(this->width / 2.0f - PLAYER_SIZE.x / 2.0f, this->height - PLAYER_SIZE.y);
+    player = new Game_Object(playerPos, PLAYER_SIZE, Resource_Manager::get_texture("skull") );
 }
+
 
 void Game::update(float dt)
 {
@@ -40,10 +48,99 @@ void Game::update(float dt)
 
 void Game::proccess_input(float dt)
 {
-    
+    SDL_Event event;
+    if (this->State == GAME_ACTIVE)
+    {
+        float velocity = PLAYER_VELOCITY * dt;
+        /*if (this->keys[SDL_SCANCODE_A])
+        {
+            if (player->position.x >= 0.0f)
+            {
+                player->position.x -= velocity;
+                player->rotation = 270.0f;;
+            }
+                
+        }
+        if (this->keys[SDL_SCANCODE_D])
+        {
+            if (player->position.x <= this->width - player->size.x)
+            {
+                player->position.x += velocity;
+                player->rotation = 90.0f;
+            }
+                
+        }
+        if (this->keys[SDL_SCANCODE_S])
+        {
+            if (player->position.y <= this->height - player->size.y)
+            {
+                player->position.y += velocity;
+                player->rotation = 180.0f;
+            }
+                
+        }
+        if (this->keys[SDL_SCANCODE_W])
+        {
+            if (player->position.y >= 0.0f)
+            {
+                player->position.y += velocity;
+                player->rotation = 0.0f;
+            }
+                
+        }*/
+
+        while(SDL_PollEvent(&event))
+        {
+            switch(event.type)
+            {
+                case SDL_EVENT_KEY_DOWN:
+                    switch (event.key.keysym.sym)
+                    {
+                        case SDL_SCANCODE_A:
+                            if (player->position.x >= 0.0f)
+                            {
+                                player->position.x -= velocity;
+                                player->rotation = 270.0f;;
+                            }
+                            break;
+                        case SDL_SCANCODE_D:
+                            if (player->position.x <= this->width - player->size.x)
+                            {
+                                player->position.x += velocity;
+                                player->rotation = 90.0f;
+                            }
+                            break;
+                        case SDL_SCANCODE_W:
+                            if (player->position.y >= 0.0f)
+                            {
+                                player->position.y += velocity;
+                                player->rotation = 0.0f;
+                            }
+                            break;
+                        case SDL_SCANCODE_S:
+                            if (player->position.y <= this->height - player->size.y)
+                            {
+                                player->position.y += velocity;
+                                player->rotation = 180.0f;
+                            }
+                            break;    
+                        default:
+                        break;
+                    }
+                case SDL_EVENT_QUIT:
+                    this->State = GAME_EXIT;
+                    break;
+                default:
+                    break;
+
+            }
+                
+
+        }
+    }
 }
 
 void Game::render()
 {
-    renderer->draw_sprite(Resource_Manager::get_texture("skull"), glm::vec2(100.0f, 200.0f), glm::vec2(360.0f, 360.0f), 180.0f, glm::vec3(0.0f, 0.0f, 0.0f));
+    player->Draw(*renderer);
 }
--- 06-textures/01-texture-engine/game.hpp
+++ 06-textures/01-texture-engine/game.hpp
@@ -2,12 +2,17 @@
 
 #include "glad/glad.h"
 #include <SDL3/SDL.h>
+#include <glm/glm.hpp>
 
 enum GameState{
     GAME_ACTIVE,
     GAME_MENU,
-    GAME_WIN
+    GAME_WIN,
+    GAME_EXIT
 };
+
+const glm::vec2 PLAYER_SIZE(100.0f, 20.0f);
+const float PLAYER_VELOCITY(500.0f);
 
 class Game{
     public:
